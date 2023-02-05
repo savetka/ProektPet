7c083d1734f0e64be7a348ecc2c12d34094d6611
savetka <savetkaantovski22@gmail.com>
2023-02-05 21:19:27 +0100

    updated PetaKlasa

--- src/com/company/PetaKlasa.java
+++ src/com/company/PetaKlasa.java
@@ -9,6 +9,8 @@ public class PetaKlasa {
         System.out.println("Hello world");
         System.out.println("Sara");
         sayHello("Daniel");
+
+        System.out.println("Learning");
     }
 
     public static void sayHello(String name){
