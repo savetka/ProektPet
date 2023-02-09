e242cba65258bea3ed346a2d5fa6023516622a38
savetka <savetkaantovski22@gmail.com>
2023-02-09 11:19:59 +0100

    login test

---
+++ src/com/company/AutotestClass.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
+public class AutotestClass {
+    public static void main(String[] args){
+
+        System.out.println("Logintest");
+    }
+
+    public static void Logintest(String name ){
+
+        System.out.println("Logintest" +name);
+
+
+    }
+}
